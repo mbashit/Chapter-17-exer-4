7bcc61b4746de436dde87322b4343b1031495ff4
Bashitapwa Michael <michaelbashitapwa52@gmail.com>
2022-09-25 00:58:10 +0300

    Add project files.

---
+++ Chapter 17 exer 4/Chapter 17 exer 4.cpp
@@ -0,0 +1,29 @@
+// Chapter 17 exer 4.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+/*Two football teams play against each other in the UEFA Champions
+League. Write a C++ program that prompts the user to enter the names
+of the two teams and the goals that each team scored, and then displays
+the name of the winner. Assume that the user enters valid values and
+there is no tie (draw).*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int goalA,goalB;
+	string teamA,teamB;
+
+	cout << "enter the names of the two teams";
+	cin >> teamA >> teamB;
+	 
+	cout << "enter the goals scored by each of the teams";
+	cin >> goalA >> goalB;	
+
+	if (goalA > goalB) {
+		cout << "the winner is" << goalA << endl;
+	}
+	return 0;
+}
+
+
